c1457820638f1571ef1513d76941b1e7c3a9a128
김현종 <dw3510@naver.com>
2023-01-27 17:48:01 +0900

    230127

--- algorithm/baekjoon/---11653.py
+++ algorithm/baekjoon/---11653.py
@@ -1,17 +1,23 @@
-import sys
-input = sys.stdin.readline
+# import sys
+# input = sys.stdin.readline
 
-def is_prime(n):
-    if n == 2:
-        return True
-    if n == 1:
-        return False
-    for i in range(2, n):
-        if n % i == 0:
-            return False
-    return True
+# def is_prime(n):
+#     if n == 1:
+#         return False
+#     for i in range(2, int(n**0.5 +1)):
+#         if n % i == 0:
+#             return False
+#     return True
 
-N = int(input())
+# N = int(input())
+# i = 2
+# while N != 1:
+#     if is_prime(i) and N % i == 0:
+#         N = N//i
+#         print(i)
+#     else:
+#         i += 1    
 
-#몫이 1이 될때까지 반복
-#나머지가 0이면서 소수인 수로 나누기 반복
+#--------------------------시간초과-------------------------
+
+#1. prime 판정 변화

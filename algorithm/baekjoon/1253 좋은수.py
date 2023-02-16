88fdde0b93bb2dd6fdf40c9de0f4010e130e9227
김현종 <starringbell@gmail.com>
2023-02-16 17:57:22 +0900

    230216

--- algorithm/baekjoon/1253 좋은수.py
+++ algorithm/baekjoon/1253 좋은수.py
@@ -1,4 +1,22 @@
 N = int(input())
 A = list(map(int, input().split()))
 A.sort()
+cnt = 0
+if N<=2:
+    cnt = 0
+else:
+    for i in range(2, len(A)):
+        s = 0
+        e = i-1
+        while True:
+            if A[s] + A[e] < A[i]:
+                s += 1
+            elif A[s] + A[e] > A[i]:
+                s -= 1
+            else:
+                cnt += 1
+                break
+            if s == e:
+                break
 
+print(cnt)
